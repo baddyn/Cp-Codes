576551f4e3f27924b130682fa4d2ab973192a30f
baddyn <52738681+baddyn@users.noreply.github.com>
2020-04-08 06:21:15 +0530

    Update pbds.cpp

--- pbds.cpp
+++ pbds.cpp
@@ -1,9 +1,13 @@
 #include<bits/stdc++.h>
 #define ll  long long int
+#define ff first
+#define ss second
+#define mp make_pair
 #include <ext/pb_ds/assoc_container.hpp> 
 #include <ext/pb_ds/tree_policy.hpp> 
 using namespace std;
 using namespace __gnu_pbds;
+#define pbdm tree<pair<ll,ll>, null_type, less<pair<ll,ll> >, rb_tree_tag, tree_order_statistics_node_update> 
 #define pbds tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> 
 
 int main()
@@ -34,5 +38,32 @@ int main()
     cout<<endl;
     
     cout<<*s.upper_bound(16)<<endl<<*s.lower_bound(16)<<endl;
+    
+    
+    //pbdm now
+    
+    pbdm ms;
+    
+    for(ll i=1;i<8;i++)
+    ms.insert(mp(i*i,i));
+    
+     ms.insert(mp(4,8));
+      ms.insert(mp(4,9));
+    
+    for(auto i:ms)
+    cout<<i.ff<<" "<<i.ss<<endl;
+   
+    cout<<ms.size()<<endl;
+    
+    ms.erase(ms.lower_bound({4,0}));
+    
+    cout<<ms.size()<<endl;
+    
+    //cout<<*ms.find_by_order(2)<<endl;
+    cout<<ms.order_of_key({17,0})<<endl;
+    
+    cout<<ms.find_by_order(2)->ff;
+    
+
  
 }
